d01fc38264218177b21430af907046a21ab2b865
Rain Shen <slk49@live.cn>
2018-02-08 14:49:37 -0500

    css implementation of slideshow

--- 2018src/sponsors.js
+++ 2018src/sponsors.js
@@ -1,34 +1,12 @@
-/* global detectOverflowY,Promises,cardloaded,loadLazyImg */
+/* global,Promises,loadLazyImg,waitDOMLoad */
 (async function() {
   const sponsors = $(".sponsors");
   const imgs = $$("img", sponsors);
-  await cardloaded;
+  await waitDOMLoad();
   await new Promises(
     imgs.map(loadLazyImg)
   );
-  // console.log(last.getBoundingClientRect().right - first.getBoundingClientRect().left, sponsors.getBoundingClientRect().width)
-  if (detectOverflowY(sponsors)) {
-    // overflowed, need to run
-    // console.log('running sponsors')
-    carousel(sponsors);
-  }
+  imgs.forEach(img=>sponsors.appendChild(img.parentNode.cloneNode(true)))
+  sponsors.style.animationPlayState="running";
 })();
-function carousel(sponsors) {
-  const first = sponsors.firstElementChild;
-  function scroll(lastx, dir) {
-    if (isInViewport(sponsors)) {
-      sponsors.scrollLeft += dir;
-    } else {
-      console.log("not in viewport");
-    }
-    if (lastx === sponsors.scrollLeft) {
-      setTimeout(() => requestAnimationFrame(() => scroll(lastx, -dir)), 2000);
-    } else {
-      requestAnimationFrame(() =>
-        requestAnimationFrame(() => scroll(sponsors.scrollLeft, dir))
-      );
-    }
-  }
-  requestAnimationFrame(() => scroll(first.getBoundingClientRect(), 2));
-}
 
